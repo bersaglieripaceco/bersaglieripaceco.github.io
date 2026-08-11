494b464565c1f443fcddcb8468c0fe3fff1056eb
bersaglieripaceco <anbpaceco@gmail.com>
2026-08-11 22:35:51 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,31 @@
-// Progetto Bersaglieri Paceco
+document.addEventListener("DOMContentLoaded", function () {
+
+    const contenitoreMenu = document.getElementById("menu-comune");
+
+    if (!contenitoreMenu) {
+        return;
+    }
+
+    fetch("../menu.html")
+        .then(function (risposta) {
+
+            if (!risposta.ok) {
+                throw new Error("Impossibile caricare il menu.");
+            }
+
+            return risposta.text();
+
+        })
+        .then(function (menu) {
+
+            contenitoreMenu.innerHTML = menu;
+
+        })
+        .catch(function (errore) {
+
+            console.error("Errore nel caricamento del menu:", errore);
+
+        });
+
+});// Progetto Bersaglieri Paceco
 // Script iniziale// JavaScript Document
